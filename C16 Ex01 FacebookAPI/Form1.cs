0a154669551625fc5876ee3447fe0d5a036359e4
liornaar <liornaar1@gmail.com>
2016-08-07 18:43:57 +0300

    facebook login created

--- C16 Ex01 FacebookAPI/Form1.cs
+++ C16 Ex01 FacebookAPI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FacebookWrapper;
 
 namespace C16_Ex01_FacebookAPI
 {
@@ -15,6 +16,17 @@ namespace C16_Ex01_FacebookAPI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoginResult result = FacebookWrapper.FacebookService.Login("2287434634730573", 
+                "public_profile", "user_friends", "email");
         }
     }
 }
