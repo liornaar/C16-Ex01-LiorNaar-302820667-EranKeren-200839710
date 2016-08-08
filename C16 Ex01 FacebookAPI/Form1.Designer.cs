e2704f986b0b788743e1608dead363a2c32eae2c
liornaar <liornaar1@gmail.com>
2016-08-08 21:46:28 +0300

    add some components

--- C16 Ex01 FacebookAPI/Form1.Designer.cs
+++ C16 Ex01 FacebookAPI/Form1.Designer.cs
@@ -28,35 +28,347 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.coverPicture = new System.Windows.Forms.PictureBox();
+            this.profilePicture = new System.Windows.Forms.PictureBox();
+            this.rememberMeCheckBox = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.postLabel = new System.Windows.Forms.Label();
+            this.postButton = new System.Windows.Forms.Button();
+            this.eventPicture1 = new System.Windows.Forms.PictureBox();
+            this.eventPicture2 = new System.Windows.Forms.PictureBox();
+            this.eventPicture3 = new System.Windows.Forms.PictureBox();
+            this.eventPicture4 = new System.Windows.Forms.PictureBox();
+            this.eventPicture5 = new System.Windows.Forms.PictureBox();
+            this.inLabel1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.eventLocation1 = new System.Windows.Forms.Label();
+            this.eventLocation2 = new System.Windows.Forms.Label();
+            this.eventLocation3 = new System.Windows.Forms.Label();
+            this.eventLocation4 = new System.Windows.Forms.Label();
+            this.eventLocation5 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.coverPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profilePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // loginButton
             // 
-            this.button1.Location = new System.Drawing.Point(586, 29);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.loginButton.Location = new System.Drawing.Point(372, 46);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(171, 40);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // coverPicture
+            // 
+            this.coverPicture.Location = new System.Drawing.Point(0, 1);
+            this.coverPicture.Name = "coverPicture";
+            this.coverPicture.Size = new System.Drawing.Size(877, 190);
+            this.coverPicture.TabIndex = 1;
+            this.coverPicture.TabStop = false;
+            this.coverPicture.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // profilePicture
+            // 
+            this.profilePicture.Location = new System.Drawing.Point(29, 122);
+            this.profilePicture.Name = "profilePicture";
+            this.profilePicture.Size = new System.Drawing.Size(133, 126);
+            this.profilePicture.TabIndex = 2;
+            this.profilePicture.TabStop = false;
+            this.profilePicture.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // rememberMeCheckBox
+            // 
+            this.rememberMeCheckBox.AutoSize = true;
+            this.rememberMeCheckBox.Location = new System.Drawing.Point(372, 92);
+            this.rememberMeCheckBox.Name = "rememberMeCheckBox";
+            this.rememberMeCheckBox.Size = new System.Drawing.Size(122, 21);
+            this.rememberMeCheckBox.TabIndex = 3;
+            this.rememberMeCheckBox.Text = "Remember me";
+            this.rememberMeCheckBox.UseVisualStyleBackColor = true;
+            this.rememberMeCheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 206);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // postLabel
+            // 
+            this.postLabel.AutoSize = true;
+            this.postLabel.Location = new System.Drawing.Point(212, 209);
+            this.postLabel.Name = "postLabel";
+            this.postLabel.Size = new System.Drawing.Size(48, 17);
+            this.postLabel.TabIndex = 5;
+            this.postLabel.Text = "Status";
+            this.postLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // postButton
+            // 
+            this.postButton.Location = new System.Drawing.Point(523, 205);
+            this.postButton.Name = "postButton";
+            this.postButton.Size = new System.Drawing.Size(75, 29);
+            this.postButton.TabIndex = 6;
+            this.postButton.Text = "Post!";
+            this.postButton.UseVisualStyleBackColor = true;
+            // 
+            // eventPicture1
+            // 
+            this.eventPicture1.Location = new System.Drawing.Point(3, 535);
+            this.eventPicture1.Name = "eventPicture1";
+            this.eventPicture1.Size = new System.Drawing.Size(159, 130);
+            this.eventPicture1.TabIndex = 10;
+            this.eventPicture1.TabStop = false;
+            // 
+            // eventPicture2
+            // 
+            this.eventPicture2.Location = new System.Drawing.Point(190, 535);
+            this.eventPicture2.Name = "eventPicture2";
+            this.eventPicture2.Size = new System.Drawing.Size(159, 130);
+            this.eventPicture2.TabIndex = 11;
+            this.eventPicture2.TabStop = false;
+            // 
+            // eventPicture3
+            // 
+            this.eventPicture3.Location = new System.Drawing.Point(372, 535);
+            this.eventPicture3.Name = "eventPicture3";
+            this.eventPicture3.Size = new System.Drawing.Size(159, 130);
+            this.eventPicture3.TabIndex = 12;
+            this.eventPicture3.TabStop = false;
+            // 
+            // eventPicture4
+            // 
+            this.eventPicture4.Location = new System.Drawing.Point(548, 535);
+            this.eventPicture4.Name = "eventPicture4";
+            this.eventPicture4.Size = new System.Drawing.Size(159, 130);
+            this.eventPicture4.TabIndex = 13;
+            this.eventPicture4.TabStop = false;
+            // 
+            // eventPicture5
+            // 
+            this.eventPicture5.Location = new System.Drawing.Point(718, 535);
+            this.eventPicture5.Name = "eventPicture5";
+            this.eventPicture5.Size = new System.Drawing.Size(159, 130);
+            this.eventPicture5.TabIndex = 14;
+            this.eventPicture5.TabStop = false;
+            // 
+            // inLabel1
+            // 
+            this.inLabel1.AutoSize = true;
+            this.inLabel1.Location = new System.Drawing.Point(12, 672);
+            this.inLabel1.Name = "inLabel1";
+            this.inLabel1.Size = new System.Drawing.Size(25, 17);
+            this.inLabel1.TabIndex = 15;
+            this.inLabel1.Text = "At:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 672);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "At:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 672);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "At:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(562, 672);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "At:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(726, 672);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "At:";
+            // 
+            // eventLocation1
+            // 
+            this.eventLocation1.AutoSize = true;
+            this.eventLocation1.Location = new System.Drawing.Point(41, 672);
+            this.eventLocation1.Name = "eventLocation1";
+            this.eventLocation1.Size = new System.Drawing.Size(74, 17);
+            this.eventLocation1.TabIndex = 20;
+            this.eventLocation1.Text = "eventCity1";
+            // 
+            // eventLocation2
+            // 
+            this.eventLocation2.AutoSize = true;
+            this.eventLocation2.Location = new System.Drawing.Point(241, 672);
+            this.eventLocation2.Name = "eventLocation2";
+            this.eventLocation2.Size = new System.Drawing.Size(46, 17);
+            this.eventLocation2.TabIndex = 21;
+            this.eventLocation2.Text = "label5";
+            // 
+            // eventLocation3
+            // 
+            this.eventLocation3.AutoSize = true;
+            this.eventLocation3.Location = new System.Drawing.Point(430, 672);
+            this.eventLocation3.Name = "eventLocation3";
+            this.eventLocation3.Size = new System.Drawing.Size(46, 17);
+            this.eventLocation3.TabIndex = 22;
+            this.eventLocation3.Text = "label6";
+            // 
+            // eventLocation4
+            // 
+            this.eventLocation4.AutoSize = true;
+            this.eventLocation4.Location = new System.Drawing.Point(607, 672);
+            this.eventLocation4.Name = "eventLocation4";
+            this.eventLocation4.Size = new System.Drawing.Size(46, 17);
+            this.eventLocation4.TabIndex = 23;
+            this.eventLocation4.Text = "label7";
+            // 
+            // eventLocation5
+            // 
+            this.eventLocation5.AutoSize = true;
+            this.eventLocation5.Location = new System.Drawing.Point(780, 672);
+            this.eventLocation5.Name = "eventLocation5";
+            this.eventLocation5.Size = new System.Drawing.Size(46, 17);
+            this.eventLocation5.TabIndex = 24;
+            this.eventLocation5.Text = "label8";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(390, 505);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 17);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Upcoming Events";
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 254);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 268);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Posts";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(3, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(239, 244);
+            this.listBox1.TabIndex = 27;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1281, 547);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(876, 698);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.eventLocation5);
+            this.Controls.Add(this.eventLocation4);
+            this.Controls.Add(this.eventLocation3);
+            this.Controls.Add(this.eventLocation2);
+            this.Controls.Add(this.eventLocation1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inLabel1);
+            this.Controls.Add(this.eventPicture5);
+            this.Controls.Add(this.eventPicture4);
+            this.Controls.Add(this.eventPicture3);
+            this.Controls.Add(this.eventPicture2);
+            this.Controls.Add(this.eventPicture1);
+            this.Controls.Add(this.postButton);
+            this.Controls.Add(this.postLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rememberMeCheckBox);
+            this.Controls.Add(this.profilePicture);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.coverPicture);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "dw";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.coverPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profilePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicture5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.PictureBox coverPicture;
+        private System.Windows.Forms.PictureBox profilePicture;
+        private System.Windows.Forms.CheckBox rememberMeCheckBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label postLabel;
+        private System.Windows.Forms.Button postButton;
+        private System.Windows.Forms.PictureBox eventPicture1;
+        private System.Windows.Forms.PictureBox eventPicture2;
+        private System.Windows.Forms.PictureBox eventPicture3;
+        private System.Windows.Forms.PictureBox eventPicture4;
+        private System.Windows.Forms.PictureBox eventPicture5;
+        private System.Windows.Forms.Label inLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label eventLocation1;
+        private System.Windows.Forms.Label eventLocation2;
+        private System.Windows.Forms.Label eventLocation3;
+        private System.Windows.Forms.Label eventLocation4;
+        private System.Windows.Forms.Label eventLocation5;
+        private System.Windows.Forms.Label label5;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- C16 Ex01 FacebookAPI/Form1.cs
+++ C16 Ex01 FacebookAPI/Form1.cs
@@ -26,7 +26,49 @@ namespace C16_Ex01_FacebookAPI
         private void button1_Click(object sender, EventArgs e)
         {
             LoginResult result = FacebookWrapper.FacebookService.Login("2287434634730573", 
-                "public_profile", "user_friends", "email");
+                "public_profile", "user_friends", "email", "user_posts", "user_videos", "user_events", "read_stream");
+            coverPicture.LoadAsync(result.LoggedInUser.Cover.SourceURL);
+            coverPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
