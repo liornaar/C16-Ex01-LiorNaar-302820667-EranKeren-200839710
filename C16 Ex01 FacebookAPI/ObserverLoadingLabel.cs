660cbc2a64046af25299e3db90d063218a72f303
liornaar <liornaar1@gmail.com>
2016-09-26 18:09:54 +0300

    added observer pattern

---
+++ C16 Ex01 FacebookAPI/ObserverLoadingLabel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C16_Ex02_FacebookAPI
+{
+    public class ObserverLoadingLabel : IObserver<PictureBox>
+    {
+        public PictureBox m_Picturebox { get; set; }
+        public bool m_Hidden { get; set; }
+        public Image m_LoadingImage { get; set; }
+
+        public ObserverLoadingLabel(PictureBox i_Picture)
+        {
+            m_Picturebox = i_Picture;
+            m_Hidden = false;
+            m_Picturebox.Image = Image.FromFile("hourglass.gif");
+        }
+
+        public void OnNext(PictureBox value)
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+            m_Picturebox.Invoke(new Action(() => m_Picturebox.Image = Image.FromFile("error.png")));
+        }
+
+        public void OnCompleted()
+        {
+            m_Picturebox.Invoke(new Action(() => m_Picturebox.Hide()));
+        }
+    }
+}
